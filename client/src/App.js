26c76be4061996802323e0332979f6ecda135e2f
TrevorJoel <trevsstuff@hotmail.com>
2020-04-10 12:15:04 +0300

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../src/components/Layout/Layout';
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder'
+// Test
 function App() {
   return (
     <div >
